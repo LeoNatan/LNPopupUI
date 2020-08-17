a280fc64ab008e085dd22a5329a69f66a5996243
Leo Natan <LeoNatan@users.noreply.github.com>
2020-08-18 00:48:31 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
 //		.package(path: "../LNPopupController")
-		.package(url: "https://github.com/LeoNatan/LNPopupController.git", from: Version(stringLiteral: "2.9.5"))
+		.package(url: "https://github.com/LeoNatan/LNPopupController.git", from: Version(stringLiteral: "2.10.0"))
     ],
     targets: [
         .target(
