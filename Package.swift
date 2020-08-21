295b84499a0b413ed961f1e3ac52172d79b320da
Leo Natan <leo.natan@outlook.com>
2020-08-21 20:06:51 +0300

    Bump LNPopupController version

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
 //		.package(path: "../LNPopupController")
-		.package(url: "https://github.com/LeoNatan/LNPopupController.git", from: Version(stringLiteral: "2.10.0"))
+		.package(url: "https://github.com/LeoNatan/LNPopupController.git", from: Version(stringLiteral: "2.10.2"))
     ],
     targets: [
         .target(
