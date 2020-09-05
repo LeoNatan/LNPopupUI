935e0e6de1d6bf2bb51a25fb46031426a6f60afe
Leo Natan <leo.natan@outlook.com>
2020-09-06 01:55:26 +0300

    Use latest LNPopupController

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     ],
     dependencies: [
 //		.package(path: "../LNPopupController")
-		.package(url: "https://github.com/LeoNatan/LNPopupController.git", from: Version(stringLiteral: "2.10.8"))
+		.package(url: "https://github.com/LeoNatan/LNPopupController.git", from: Version(stringLiteral: "2.10.9"))
     ],
     targets: [
         .target(
