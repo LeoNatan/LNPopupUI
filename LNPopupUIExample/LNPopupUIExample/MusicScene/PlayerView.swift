315adcdd42c7bf50d5695039c602f46267882c9d
Leo Natan <leo.natan@outlook.com>
2021-09-01 22:54:27 +0300

    Fix player view in example app

--- LNPopupUIExample/LNPopupUIExample/MusicScene/PlayerView.swift
+++ LNPopupUIExample/LNPopupUIExample/MusicScene/PlayerView.swift
@@ -33,100 +33,81 @@ struct PlayerView: View {
 	
 	var body: some View {
 		GeometryReader { geometry in
-			
 			return VStack {
-				ZStack {
-					Rectangle()
-						.fill()
-						.frame(width: geometry.size.width, height: 60)
-				}
-				.popupInteractionContainer()
-				ScrollView {
-					Image(song.imageName)
-						.resizable()
-						.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
-						.aspectRatio(contentMode: .fit)
-						.padding([.leading, .trailing], 10)
-						.padding([.top], geometry.size.height * 60 / 896.0)
-						.shadow(radius: 5)
-					
-					Image(song.imageName)
-						.resizable()
-						.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
-						.aspectRatio(contentMode: .fit)
-						.padding([.leading, .trailing], 10)
-						.padding([.top], geometry.size.height * 60 / 896.0)
-						.shadow(radius: 5)
-					
-					VStack(spacing: geometry.size.height * 30.0 / 896.0) {
-						HStack {
-							VStack(alignment: .leading) {
-								Text(song.title)
-									.font(.system(size: 20, weight: .bold))
-								Text(song.subtitle)
-									.font(.system(size: 20, weight: .regular))
-							}
-							.lineLimit(1)
-							.frame(minWidth: 0,
-								   maxWidth: .infinity,
-								   alignment: .topLeading)
-							Button(action: {}, label: {
-								Image(systemName: "ellipsis.circle")
-									.font(.title)
-							})
+				Image(song.imageName)
+					.resizable()
+					.clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+					.aspectRatio(contentMode: .fit)
+					.padding([.leading, .trailing], 10)
+					.padding([.top], geometry.size.height * 60 / 896.0)
+					.shadow(radius: 5)
+				VStack(spacing: geometry.size.height * 30.0 / 896.0) {
+					HStack {
+						VStack(alignment: .leading) {
+							Text(song.title)
+								.font(.system(size: 20, weight: .bold))
+							Text(song.subtitle)
+								.font(.system(size: 20, weight: .regular))
 						}
-						if #available(iOS 14.0, *) {
-							ProgressView(value: playbackProgress)
-								.padding([.bottom], geometry.size.height * 30.0 / 896.0)
-						} else {
-							Slider(value: $playbackProgress)
-								.padding([.bottom], geometry.size.height * 30.0 / 896.0)
-						}
-						HStack {
-							Button(action: {}, label: {
-								Image(systemName: "backward.fill")
-							})
-								.frame(minWidth: 0, maxWidth: .infinity)
-							Button(action: {
-								isPlaying.toggle()
-							}, label: {
-								Image(systemName: isPlaying ? "pause.fill" : "play.fill")
-							})
-								.font(.system(size: 50, weight: .bold))
-								.frame(minWidth: 0, maxWidth: .infinity, minHeight: 50, maxHeight: 50)
-							Button(action: {}, label: {
-								Image(systemName: "forward.fill")
-							})
-								.frame(minWidth: 0, maxWidth: .infinity)
-						}
-						.font(.system(size: 30, weight: .regular))
-						.padding([.bottom], geometry.size.height * 20.0 / 896.0)
-						HStack {
-							Image(systemName: "speaker.fill")
-							Slider(value: $volume)
-							Image(systemName: "speaker.wave.2.fill")
-						}
-						.font(.footnote)
-						.foregroundColor(.gray)
-						HStack {
-							Button(action: {}, label: {
-								Image(systemName: "shuffle")
-							})
-								.frame(minWidth: 0, maxWidth: .infinity)
-							Button(action: {}, label: {
-								Image(systemName: "airplayaudio")
-							})
-								.frame(minWidth: 0, maxWidth: .infinity)
-							Button(action: {}, label: {
-								Image(systemName: "repeat")
-							})
-								.frame(minWidth: 0, maxWidth: .infinity)
-						}
-						.font(.body)
+						.lineLimit(1)
+						.frame(minWidth: 0,
+							   maxWidth: .infinity,
+							   alignment: .topLeading)
+						Button(action: {}, label: {
+							Image(systemName: "ellipsis.circle")
+								.font(.title)
+						})
 					}
-					.padding(geometry.size.height * 40.0 / 896.0)
+					if #available(iOS 14.0, *) {
+						ProgressView(value: playbackProgress)
+							.padding([.bottom], geometry.size.height * 30.0 / 896.0)
+					} else {
+						Slider(value: $playbackProgress)
+							.padding([.bottom], geometry.size.height * 30.0 / 896.0)
+					}
+					HStack {
+						Button(action: {}, label: {
+							Image(systemName: "backward.fill")
+						})
+							.frame(minWidth: 0, maxWidth: .infinity)
+						Button(action: {
+							isPlaying.toggle()
+						}, label: {
+							Image(systemName: isPlaying ? "pause.fill" : "play.fill")
+						})
+							.font(.system(size: 50, weight: .bold))
+							.frame(minWidth: 0, maxWidth: .infinity, minHeight: 50, maxHeight: 50)
+						Button(action: {}, label: {
+							Image(systemName: "forward.fill")
+						})
+							.frame(minWidth: 0, maxWidth: .infinity)
+					}
+					.font(.system(size: 30, weight: .regular))
+					.padding([.bottom], geometry.size.height * 20.0 / 896.0)
+					HStack {
+						Image(systemName: "speaker.fill")
+						Slider(value: $volume)
+						Image(systemName: "speaker.wave.2.fill")
+					}
+					.font(.footnote)
+					.foregroundColor(.gray)
+					HStack {
+						Button(action: {}, label: {
+							Image(systemName: "shuffle")
+						})
+							.frame(minWidth: 0, maxWidth: .infinity)
+						Button(action: {}, label: {
+							Image(systemName: "airplayaudio")
+						})
+							.frame(minWidth: 0, maxWidth: .infinity)
+						Button(action: {}, label: {
+							Image(systemName: "repeat")
+						})
+							.frame(minWidth: 0, maxWidth: .infinity)
+					}
+					.font(.body)
 				}
-				
+				.padding(geometry.size.height * 40.0 / 896.0)
 			}
 			.frame(minWidth: 0,
 				   maxWidth: .infinity,
